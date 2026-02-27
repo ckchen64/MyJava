d32976252d9dd842131fe172f7f2f224522f6766
ckchen64 <ckchen64@gmail.com>
2026-02-27 16:47:50 +0900

    3nd amend

--- src/ch01_variable_operator/MyPrint.java
+++ src/ch01_variable_operator/MyPrint.java
@@ -3,6 +3,6 @@ package ch01_variable_operator;
 public class MyPrint {
     static void main() {
         System.out.println("hello");
-        System.out.println("world");//kkkk
+        System.out.println("world");//abcd
     }
 }
